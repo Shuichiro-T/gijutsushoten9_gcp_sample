4566baec36dabc47fe8bae7bdad4f7c62bf949f3
Shuihiro.TAKAHASHI <shuichiro.takahashi@japan-systems.co.jp>
2020-08-13 13:48:47 +0900

    日本語コメントを追加

--- appengine/main.py
+++ appengine/main.py
@@ -12,8 +12,11 @@ def main():
     id = request.args.get('id')
     message = request.args.get('message')
 
-    query = f'INSERT INTO DATASET1.TABLE1 (ID, MESSAGE) VALUES ({id}, \"{message}\" )'
+    # データ作成用SQLの構築
+    query = 'INSERT INTO DATASET1.TABLE1 (ID, MESSAGE)' 
+        + f'VALUES ({id}, \"{message}\" )'
 
+    # SQLの実行
     bigquery_client.query(query).result()
 
     return redirect(
@@ -25,10 +28,13 @@ def main():
 @app.route("/list")
 def get_lists():
 
+    # データ取得用のSQL作成
     query = 'SELECT * FROM DATASET1.TABLE1'
 
+    # データの取得
     results = bigquery_client.query(query).result()
 
+    # 取得結果をlist.htmlへ渡す
     return render_template("list.html", results=results)
 
 
